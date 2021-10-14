8a926846959e959fc9e48dbbd4f6772bb75c35d3
kaitlyn-liao <59896595+kaitlyn-liao@users.noreply.github.com>
2021-10-14 00:03:58 -0700

    completed lab 4

---
+++ lab4/js/lab.js
@@ -0,0 +1,51 @@
+// Pseudocoding
+// Getting in the habit of pseudocoding and commenting code.
+// @link   https://kaitlyn-liao.github.io/art-101/lab4/index.html
+// @file   This files defines the MyClass class.
+// @author Kaitlyn Liao
+// @since  Oct. 13, 2021
+
+// Making Cereal 
+// 1. Get a bowl, milk, and your choice of cereal 
+// 2. Open the the cereal box and pour desired amount into the bowl
+// 3. Open milk carton and pour just enough to almost cover the cereal in the bowl
+// 4. Close up the cereal box and milk carton
+// 5. Eat with a spoon
+
+
+// Game of Rock, Paper, Scissors
+// 1. print the options: rock, paper, scissors
+// 2. accept user input 
+// 3. keep track of player and computer scores
+// 4. have computer randomly pick an option
+// 5. compare player vs. computer choice and determine who won 
+// 6. add a point to whomever won
+
+// print the options: rock, paper, scissors
+// player score starts at 0 points 
+// computer score starts at 0 points 
+// loop until the player or computer scores 5 points
+//     save players choice
+//     randomly pick rock paper scissors and save as computer choice
+//     compare player and computer choice
+//     assign point to winner 
+//     print updated score 
+// print who won
+
+// Organize To-Do List 
+// 1. accept multiple user input asking for task name and taks due date
+// 2. put list of tasks and due dates into a list 
+// 3. reorganize the list by earliest to latest due date
+// 4. print out reordered list for user to see
+
+// accept list of tasks and their due dates
+// create a new empty list to store reorganized list 
+// remove the first task in user inputted list and put it in our new list
+// loop through the elements of the new list 
+//     check if the current element's due date is before or at the same time as the first element in user list
+//         remove first element of user list and insert it at our current position in the new list 
+//     if we have reached the end of the new list 
+//         insert the first element of the user list at the end of the new list 
+// print the new list of tasks organized by due date
+
+

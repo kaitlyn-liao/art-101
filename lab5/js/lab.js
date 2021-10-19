1002a7015643a3f6a53a2c907aba2fa815844b6c
kaitlyn-liao <59896595+kaitlyn-liao@users.noreply.github.com>
2021-10-18 23:02:19 -0700

    finished lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,20 @@
+/*
+ * Author: Kaitlyn Liao <kayliao@ucsc.edu>
+ * Created: October 18, 2021
+ */
+
+// Define Variables
+make = "Toyota";
+model = "Highland";
+color = "White";
+year = 2020;
+
+// calculate
+age = 2021 - year;
+
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " year(s) <br>");
